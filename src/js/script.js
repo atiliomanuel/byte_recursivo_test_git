b74bb52c444a4a07ce9dec209737cb24ce2c109f
atiliomanuel <atilio.manuel@byterecursivo.com>
2025-08-15 16:58:57 +0100

    project mockup initialized

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+const btn_get_response = document.getElementById("btn-send-response");
+
+btn_get_response.addEventListener("click", () => {
+	processResponse();
+});
+
+function processResponse() {
+	alert("Message sucessfully sent!");
+);
+
